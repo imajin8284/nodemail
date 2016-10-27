c860eef666a27f3ee7be627de1501cb0e130dbb0
matsumoto <imajin8284@gmail.com>
2016-10-27 20:05:22 +0900

    命名少し変更

--- routes/contact/pages.ts
+++ routes/contact/pages.ts
@@ -19,21 +19,18 @@ export module RewritingPageRouter {
   //  const exception: any = new ExceptionController.Exception();
 
     router.get("/",[ (request:any, response:any):void => {
-        
-      let b = 1;
 
-        let file:any = file_utility.readfile("./public/contact.html", (error, file) => {
-            let html = file;
-                let file2:any = file_utility.readfile("./public/contact_confirmation.html", (error, file2) => {
-                    let html2 = file2;
-                response.render("index", {html: html,html2:html2});
-            });
+        let contact:any = file_utility.readfile("./public/contact.html", (error, contact) => {
+            if(!error){
+                let confirmation:any = file_utility.readfile("./public/contact_confirmation.html", (error, confirmation) => {
+                    if(!error){
+                        response.render("index", {contact: contact,confirmation:confirmation});
+                    }
+                });
+            }
         });
     }]);
-    
- //   router.get("/",[exception.page_guard, (request:any, response:any):void => {
- //       response.render("systems/contact/index", {user: request.user, message: "Back", status: 200});
- //   }]);
+
 }
 
 module.exports = RewritingPageRouter.router;
